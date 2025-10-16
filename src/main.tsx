e072f8f2ecf479ff8e0fa211e3d0035cfc5e4c26
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-10-16 17:58:52 +0300

    fix: cd

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
-import { YMaps } from "@pbe/react-yandex-maps";
-import * as Sentry from "@sentry/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import * as VKID from "@vkid/sdk";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { StoreProvider } from "src/app/providers/StoreProvider";
-import App from "./app/App.tsx";
 
 VKID.Config.init({
     app: import.meta.env.VITE_VK_SDK_APP_ID,
@@ -16,26 +10,8 @@ VKID.Config.init({
 
 const root = createRoot(document.getElementById("root")!);
 
-const queryClient = new QueryClient();
-
-if (
-    process.env.NODE_ENV == "production" &&
-    import.meta.env.VITE_SENTRY_ENABLED === "true"
-) {
-    Sentry.init({
-        dsn: import.meta.env.VITE_SENTRY_DSN,
-        sendDefaultPii: true,
-    });
-}
-
 root.render(
-    <BrowserRouter basename="/">
-        <StoreProvider>
-            <QueryClientProvider client={queryClient}>
-                <YMaps query={{ apikey: import.meta.env.VITE_YMAPS_API_KEY }}>
-                    <App />
-                </YMaps>
-            </QueryClientProvider>
-        </StoreProvider>
-    </BrowserRouter>
+    <div>
+        <h3>Hello world</h3>
+    </div>
 );
