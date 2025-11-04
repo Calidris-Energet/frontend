92a69cdb87ae79b2b567f649f3b6b3cd49dda344
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-11-04 16:11:48 +0300

    feat: Рефакторинг CI CD

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,30 @@
 import { YMaps } from "@pbe/react-yandex-maps";
-import * as Sentry from "@sentry/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import * as VKID from "@vkid/sdk";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { StoreProvider } from "src/app/providers/StoreProvider";
 import App from "./app/App.tsx";
 
-VKID.Config.init({
-    app: import.meta.env.VITE_VK_SDK_APP_ID,
-    redirectUrl: import.meta.env.VITE_VK_ID_REDIRECT_URL,
-    scope: "email",
-    responseMode: VKID.ConfigResponseMode.Callback,
-});
+// VKID.Config.init({
+//     app: import.meta.env.VITE_VK_SDK_APP_ID,
+//     redirectUrl: import.meta.env.VITE_VK_ID_REDIRECT_URL,
+//     scope: "email",
+//     responseMode: VKID.ConfigResponseMode.Callback,
+// });
 
 const root = createRoot(document.getElementById("root")!);
 
 const queryClient = new QueryClient();
 
-if (
-    process.env.NODE_ENV == "production" &&
-    import.meta.env.VITE_SENTRY_ENABLED === "true"
-) {
-    Sentry.init({
-        dsn: import.meta.env.VITE_SENTRY_DSN,
-        sendDefaultPii: true,
-    });
-}
+// if (
+//     process.env.NODE_ENV == "production" &&
+//     import.meta.env.VITE_SENTRY_ENABLED === "true"
+// ) {
+//     Sentry.init({
+//         dsn: import.meta.env.VITE_SENTRY_DSN,
+//         sendDefaultPii: true,
+//     });
+// }
 
 root.render(
     <BrowserRouter basename="/">
