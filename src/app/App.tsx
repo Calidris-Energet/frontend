3bc19379bf2e3e87099dd44ae8ae317280e6ed39
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-11-04 14:39:51 +0300

    Revert "feat: Рефакторинг CI CD"
    This reverts commit 45a46143a05a5158ec366314f6fd55611577fd78.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -64,7 +64,6 @@ function App() {
                 </Box>
             ) : (
                 <div className={styles.root}>
-                    <h3>asdf</h3>
                     <Header />
                     <AppRouter />
                 </div>
