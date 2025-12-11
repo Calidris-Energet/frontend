2b54cf7c6b617d7a1a7549066661342482c17ead
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-12-12 00:08:44 +0300

    feat: allure

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+import { allureCypress } from "allure-cypress/reporter";
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+    e2e: {
+        baseUrl: "http://localhost:3000",
+        setupNodeEvents(on, config) {
+            allureCypress(on, config, {
+                resultsDir: "allure-results",
+            });
+            return config;
+        },
+    },
+});
