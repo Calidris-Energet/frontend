30836e048f1f9b25ae771e5ebecf9f31b3de4f8d
Yaroslav Mihalev <j.mihalev1018@yandex.ru>
2025-12-13 15:26:35 +0300

    feat: refactor cypress

--- cypress/support/utils/mock-utils.ts
+++ cypress/support/utils/mock-utils.ts
@@ -8,19 +8,6 @@ export class MockUtils {
             statusCode: response.statusCode,
             body: response.body,
             headers: response.headers,
-        }).as(alias || this.generateAlias(method, url));
-    }
-
-    static interceptMultiple(mocks: RouteMock[]) {
-        mocks.forEach((mock) => this.intercept(mock));
-    }
-
-    static generateAlias(method: string, url: string): string {
-        const urlPart = url.replace(/[^a-zA-Z0-9]/g, "_");
-        return `${method.toLowerCase()}_${urlPart}`;
-    }
-
-    static waitForAll(aliases: string[]) {
-        aliases.forEach((alias) => cy.wait(`@${alias}`));
+        }).as(alias);
     }
 }
